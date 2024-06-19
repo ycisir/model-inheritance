1c7f9f20df64817482f21c7caadc451f020a2684
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2024-06-19 11:34:32 +0530

    Add files via upload

---
+++ proxy_model/school/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import ExamCenter, MyExamCenter
+
+@admin.register(ExamCenter)
+class ExamCenterAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cname', 'city']
+
+
+@admin.register(MyExamCenter)
+class MyExamCenterAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cname', 'city']
