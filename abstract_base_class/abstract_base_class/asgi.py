10218e4f687bd44f1a00b13f3988868c388ec2d8
Mohammad Yasir <68849840+ycisir@users.noreply.github.com>
2024-06-19 09:50:15 +0530

    Add files via upload

---
+++ abstract_base_class/abstract_base_class/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for abstract_base_class project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'abstract_base_class.settings')
+
+application = get_asgi_application()
---
+++ abstract_base_class/school/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=70)
+    age = models.IntegerField()
+    date = models.DateField()
+
+    class Meta:
+        abstract = True
+
+class Student(CommonInfo):
+    fees = models.IntegerField()
+    date = None
+
+class Teacher(CommonInfo):
+    salary = models.IntegerField()
+
+class Contractor(CommonInfo):
+    date = models.DateTimeField()
+    payment = models.IntegerField()
